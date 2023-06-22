7e3f32fa73ae94c168e7ce83a058fc591ebf7fe8
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-21 22:04:59 -0400

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,24 +1,27 @@
-#!/usr/bin/python
-""" holds class Review"""
-import models
+#!/usr/bin/python3
+"""Module for User class."""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
 
-
-class Review(BaseModel, Base):
-    """Representation of Review """
-    if models.storage_t == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+class User(BaseModel, Base):
+    """This is the class reprensentation for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
