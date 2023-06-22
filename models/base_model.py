b0caad3f4d5629da86e0af3cc343f6779fd89960
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-21 22:30:38 -0400

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,9 +7,13 @@ from datetime import datetime
 import models
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
-import uuid
+from uuid import uuid4
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
+
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
