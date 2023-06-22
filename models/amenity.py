3ea4c3e6f8743baf00579074065d8a2819272416
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-21 22:32:08 -0400

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
 """ holds class Amenity"""
-import models
 from models.base_model import BaseModel, Base
+from models.city import City
+from models.user import User
+from models.place import place_amenity
 from os import getenv
 import sqlalchemy
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from os import environ
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
