36844f528f944800057eb583251132cf8c082fb9
Bright Effah <99838742+KBryt@users.noreply.github.com>
2023-06-24 19:50:54 -0400

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python
-""" holds class City"""
+""" City Module for HBNB project """
+import sqlalchemy
 import models
-from models.place import Place
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
 
 class City(BaseModel, Base):
-    """Representation of city """
-    if models.storage_t == "db":
-        __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    """ The city class"""
+    if models.storage == "database":
+        __tablename__ = "cities"
+        state_id = Column(String(60), ForeignKey("states.id",
+                          ondelete="CASCADE"), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="cities")
+        places = relationship("Place", cascade="all, delete", backref="cities")
     else:
         state_id = ""
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes city"""
+        """ Initialize City """
         super().__init__(*args, **kwargs)
